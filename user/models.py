9f830fda75cee9407dd0e00b76fc08b9d6c143c4
Azwad22 <mdazwad22@gmail.com>
2024-01-05 10:44:00 +0600

    [user] inherited AbstractUser model to define User model with is_parent, is_child and parent_id fields

--- user/models.py
+++ user/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    is_parent = models.BooleanField(default=False)
+    is_child = models.BooleanField(default=False)
+    parent_id = models.ForeignKey('self', null=True, blank=True, on_delete=models.SET_NULL, related_name='childs')
+    
